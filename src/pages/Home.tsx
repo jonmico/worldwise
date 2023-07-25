14913cd74f9268a83aa15a9d114b4ec1b31b2afe
Jon Mico <jonmico@gmail.com>
2023-07-25 00:00:28 -0400

    Add PageNav.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,12 @@
+import { Link } from 'react-router-dom';
+import PageNav from '../components/PageNav';
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <PageNav />
+      <h1>Worldwise</h1>
+      <Link to='/pricing'>Pricing</Link>
+    </div>
+  );
 }
