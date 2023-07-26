0e6f1f4779f45e50e29ae4756814c9b1f3d8a238
Jon Mico <jonmico@gmail.com>
2023-07-26 11:30:03 -0400

    Add starter files.

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Product from './pages/Product';
 import Pricing from './pages/Pricing';
 import Home from './pages/Home';
+import AppLayout from './pages/AppLayout';
 import PageNotFound from './pages/PageNotFound';
 
 export default function App() {
@@ -11,6 +12,7 @@ export default function App() {
         <Route path='/' element={<Home />} />
         <Route path='product' element={<Product />} />
         <Route path='pricing' element={<Pricing />} />
+        <Route path='app' element={<AppLayout />} />
         <Route path='*' element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
