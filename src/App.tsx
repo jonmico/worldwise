cc6c4b66c799dcf3f39417db207baabd9ab71d89
Jon Mico <jonmico@gmail.com>
2023-07-26 12:11:19 -0400

    Add PageNav to all pages, add Login route.

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Pricing from './pages/Pricing';
 import Home from './pages/Home';
 import AppLayout from './pages/AppLayout';
 import PageNotFound from './pages/PageNotFound';
+import Login from './pages/Login';
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
         <Route path='/' element={<Home />} />
         <Route path='product' element={<Product />} />
         <Route path='pricing' element={<Pricing />} />
+        <Route path='login' element={<Login />} />
         <Route path='app' element={<AppLayout />} />
         <Route path='*' element={<PageNotFound />} />
       </Routes>
