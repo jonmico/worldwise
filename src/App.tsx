d1ead66b990d1e3a3e013dd0a09b52eda7a5a42f
Jon Mico <jonmico@gmail.com>
2023-08-11 23:18:10 -0400

    Add ProtectedRoute.

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import City from './components/City';
 
 import Form from './components/Form';
 import { AuthProvider } from './contexts/FakeAuthContext';
+import ProtectedRoute from './pages/ProtectedRoute';
 
 export default function App() {
   return (
@@ -25,7 +26,14 @@ export default function App() {
             <Route path='product' element={<Product />} />
             <Route path='pricing' element={<Pricing />} />
             <Route path='login' element={<Login />} />
-            <Route path='app' element={<AppLayout />}>
+            <Route
+              path='app'
+              element={
+                <ProtectedRoute>
+                  <AppLayout />
+                </ProtectedRoute>
+              }
+            >
               <Route index element={<Navigate replace to={'cities'} />} />
               <Route path='cities' element={<CityList />} />
               <Route path='cities/:id' element={<City />} />
